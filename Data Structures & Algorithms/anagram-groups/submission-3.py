87ba1c83222815028849b4a40703f7a6adcbbf55
siddapraveen1-darklight <siddapraveen1@gmail.com>
2026-06-25 21:11:44 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        hm = {}
+        for i in strs:
+            li=[0]*26
+            for j in i:
+                li[ord(j)-97]+=1
+            print(li)
+            st=""
+            for k in li:
+                if k==0:
+                    st+="*";
+                else:
+                    k=""+str(k)
+                    st+=k
+                    st+="*"
+            if st in hm:
+                prev = hm[st]
+                prev.append(i)
+                hm[st]=prev
+            else:
+                hm[st]=[i]
+        out=[]
+        return list(hm.values())
+        
+
+
+        
+        
